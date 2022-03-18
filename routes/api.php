5c263afde16ef89bcbdb64c54d334b8e2523d244
Tony Hart <tonyzoof@gmail.com>
2022-03-17 22:29:33 -0400

    Init

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::get('/', function () {
+        [, $query] = explode('?', request()->fullUrl() . '?');
+        return Http::withHeaders([
+            'Authorization' => request()->header('Authorization')
+        ])
+            ->get("https://data.cityofchicago.org/resource/5hsj-q4xe.json?$query")
+            ->json();
+    });
+
+    Route::get('/postal', function () {
+        [, $query] = explode('?', request()->fullUrl() . '?');
+
+        $data = Http::withHeaders([
+            'Authorization' => request()->header('Authorization')
+        ])
+            ->get("https://data.cityofchicago.org/resource/unjd-c2ca.json?$query")
+            ->json();
+
+        foreach ($data as &$d) {
+            unset($d['the_geom']);
+            $d['count'] = count($data);
+        }
+
+        return $data;
+    });
+});
